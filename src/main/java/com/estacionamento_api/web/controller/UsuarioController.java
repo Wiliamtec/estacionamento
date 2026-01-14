8ae0f5b47dcd33d0d81786e22a8d4078c5315e6f
Wiliam Reis <wiliamsup83@gmail.com>
2026-01-14 19:26:38 -0300

    Usando Bean Validation no DTO de Alteração de Senha do Usuario

--- src/main/java/com/estacionamento_api/web/controller/UsuarioController.java
+++ src/main/java/com/estacionamento_api/web/controller/UsuarioController.java
@@ -37,7 +37,7 @@ public class UsuarioController {
 
     //PatchMapping -> usado para alterações parciais
     @PatchMapping("/{id}")
-    public ResponseEntity<Void> updatePassword(@PathVariable Long id,@RequestBody UsuarioSenhaDto dto){
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id,@Valid @RequestBody UsuarioSenhaDto dto){
         Usuario user = usuService.editarSenha(id, dto.getSenhaAtual(),dto.getNovaSenha(),dto.getConfirmaSenha());
         return  ResponseEntity.noContent().build();
 
