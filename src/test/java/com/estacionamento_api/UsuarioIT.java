9a2c6ec9ad9c55928a7053c2c536caa7ca81ff65
Wiliam Reis <wiliamsup83@gmail.com>
2026-01-15 17:00:20 -0300

    Configuração de Teste de Ponta a Ponta para criar um novo usuario

---
+++ src/test/java/com/estacionamento_api/UsuarioIT.java
@@ -0,0 +1,58 @@
+package com.estacionamento_api;
+
+
+import com.estacionamento_api.web.dto.UsuarioCreateDto;
+
+import com.estacionamento_api.web.dto.UsuarioResponseDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+
+// ###############ATENÇÂO##################
+//SE ATENTAR DOCUMENTAÇÂO de Migração de Testes de Integração(Spring Boot 3 → Spring Boot 4)
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql",executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class UsuarioIT {
+
+    @LocalServerPort
+    int port;
+
+
+    WebTestClient testClient;
+
+    @BeforeEach
+    void setUp() {
+        this.testClient = WebTestClient
+                .bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .build();
+    }
+
+
+
+    @Test
+    public void createUsuario_ComUsernameEPasswordValidos_RetornarUsuarioCriadoComStatus201() {
+        UsuarioResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("tody@email.com", "123456"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UsuarioResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("tody@email.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE");
+    }
+}
---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO USUARIOS(id,username,password,role)values(100,'ana@gmail.com','456789','ROLE_ADMIN');
+INSERT INTO USUARIOS(id,username,password,role)values(101,'bia@gmail.com','789555','ROLE_CLIENTE');
+INSERT INTO USUARIOS(id,username,password,role)values(102,'tosh@gmail.com','569852','ROLE_CLIENTE')
