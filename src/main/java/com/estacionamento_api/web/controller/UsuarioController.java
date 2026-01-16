5ffd5add115be3db5eda846a303021b3fd7df6c1
Wiliam Reis <wiliamsup83@gmail.com>
2026-01-16 15:05:59 -0300

    Permissão de acesso para administrador buscar por Id

--- src/main/java/com/estacionamento_api/web/controller/UsuarioController.java
+++ src/main/java/com/estacionamento_api/web/controller/UsuarioController.java
@@ -17,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.spi.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -54,6 +55,7 @@ public class UsuarioController {
                             content = @Content(mediaType = "application/json",schema = @Schema(implementation =ErrorMessage.class )))
             })
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id){
         Usuario user = usuService.buscarPorId(id);
         return  ResponseEntity.ok(UsuarioMapper.toDto(user));
