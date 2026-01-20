16244886d42bb151f43356ce7f8cbc0feab6fe16
Wiliam Reis <wiliamsup83@gmail.com>
2026-01-20 15:24:46 -0300

    Criação da Classe EstacionamentoController e CheckIn de Veiculo

---
+++ src/main/java/com/estacionamento_api/web/controller/EstacionamentoController.java
@@ -0,0 +1,40 @@
+package com.estacionamento_api.web.controller;
+
+import com.estacionamento_api.entity.ClienteVaga;
+import com.estacionamento_api.service.EstacionamentoService;
+import com.estacionamento_api.web.dto.EstacionamentoCreateDto;
+import com.estacionamento_api.web.dto.EstacionamentoResponseDto;
+import com.estacionamento_api.web.dto.mapper.ClienteVagaMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/estacionamento")
+public class EstacionamentoController {
+
+    private final EstacionamentoService estacionamentoService;
+
+    @PostMapping("/check-in")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<EstacionamentoResponseDto> checkIn(@RequestBody @Valid EstacionamentoCreateDto dto){
+        ClienteVaga clienteVaga = ClienteVagaMapper.toClienteVaga(dto);
+        estacionamentoService.checkIn(clienteVaga);
+        EstacionamentoResponseDto responseDto = ClienteVagaMapper.toDto(clienteVaga);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequestUri().path("/{recibo}")
+                .buildAndExpand(clienteVaga.getRecibo())
+                .toUri();
+        return ResponseEntity.created(location).body(responseDto);
+
+    }
+}
