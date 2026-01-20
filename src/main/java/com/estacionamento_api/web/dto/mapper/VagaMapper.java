0e0165c48b2b343af4eb1af96438d5049c87ad1b
Wiliam Reis <wiliamsup83@gmail.com>
2026-01-19 21:45:14 -0300

    Adicionando os DTOs e Mapper para Vagas

---
+++ src/main/java/com/estacionamento_api/web/dto/mapper/VagaMapper.java
@@ -0,0 +1,20 @@
+package com.estacionamento_api.web.dto.mapper;
+
+import com.estacionamento_api.entity.Vaga;
+import com.estacionamento_api.web.dto.VagaCreateDto;
+import com.estacionamento_api.web.dto.VagaResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class VagaMapper {
+
+    public static Vaga toVaga(VagaCreateDto dto){
+        return new ModelMapper().map(dto,Vaga.class);
+    }
+
+    public static VagaResponseDto toDto(Vaga vaga){
+        return new ModelMapper().map(vaga,VagaResponseDto.class);
+    }
+}
