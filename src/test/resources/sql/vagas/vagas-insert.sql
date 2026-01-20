a573c8c157932fc1eb96b13c87941f179e508847
Wiliam Reis <wiliamsup83@gmail.com>
2026-01-19 23:06:30 -0300

    Testando recurso para inserir status(200,409,422) e para localizar por codigo status(200,204)

---
+++ src/test/resources/sql/vagas/vagas-insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO USUARIOS(id,username,password,role)values(100,'ana@gmail.com','$2y$10$BmOytATYZangNScORy75W.wK/X0dF6/7FiWy5SXRMNRp5EUicrvIi','ROLE_ADMIN');
+INSERT INTO USUARIOS(id,username,password,role)values(101,'bia@gmail.com','$2y$10$BmOytATYZangNScORy75W.wK/X0dF6/7FiWy5SXRMNRp5EUicrvIi','ROLE_CLIENTE');
+INSERT INTO USUARIOS(id,username,password,role)values(102,'tosh@gmail.com','$2y$10$BmOytATYZangNScORy75W.wK/X0dF6/7FiWy5SXRMNRp5EUicrvIi','ROLE_CLIENTE');
+
+INSERT INTO VAGAS(id,codigo,status)values(10,'A-01','LIVRE');
+INSERT INTO VAGAS(id,codigo,status)values(20,'A-02','LIVRE');
+INSERT INTO VAGAS(id,codigo,status)values(30,'A-03','OCUPADA');
+INSERT INTO VAGAS(id,codigo,status)values(40,'A-04','LIVRE');
