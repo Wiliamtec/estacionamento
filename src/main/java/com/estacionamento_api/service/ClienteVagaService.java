ecb7bc03e81341e87a92cf0d7e8f29af7f1e5cc6
Wiliam Reis <wiliamsup83@gmail.com>
2026-01-20 21:52:58 -0300

    Adicionando recurso para localizar dados de check-in pelo recibo

--- src/main/java/com/estacionamento_api/service/ClienteVagaService.java
+++ src/main/java/com/estacionamento_api/service/ClienteVagaService.java
@@ -1,6 +1,7 @@
 package com.estacionamento_api.service;
 
 import com.estacionamento_api.entity.ClienteVaga;
+import com.estacionamento_api.exception.EntityNotFoundException;
 import com.estacionamento_api.repository.ClienteVagaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,12 @@ public class ClienteVagaService {
     @Transactional
     public ClienteVaga salvar (ClienteVaga clienteVaga){
         return vagaRepository.save(clienteVaga);
+    }
+
+    @Transactional(readOnly = true)
+    public ClienteVaga buscarPorRecibo(String recibo) {
+        return vagaRepository.findByReciboAndDataSaidaIsNull(recibo).orElseThrow(
+                ()-> new EntityNotFoundException(String.format("Recibo '%s' não encontrado no sistema ou check-out ja realizado",recibo))
+        );
     }
 }
